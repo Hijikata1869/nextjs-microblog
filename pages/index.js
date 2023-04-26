bcd77cbd90158c8d559bbccffd959caa1d239285
Hijikata1869 <liv.knk@gmail.com>
2023-04-26 22:57:27 +0900

    ブログが完成

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,53 @@ import styles from '../styles/Home.module.css'
 import Layout from '../components/Layout'
 
 import Link from 'next/link'
+import utilStyle from "../styles/utils.module.css";
+import { getPostsData }  from '../lib/post';
 
-export default function Home() {
-  return (
-    <Layout></Layout>
-  );
+// SSGの場合, getStaticPropsを使う
+export async function getStaticProps() {
+  const allPostsData = getPostsData();
+  console.log(allPostsData);
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
+
+// SSRの場合
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       // コンポーネントに渡すためのprops
+//     };
+//   };
+// }
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <p>私はリヴァプールファンです。</p>
+      </section>
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage} />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <a className={utilStyle.boldText}>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))};
+        </div>
+      </section>
+    </Layout>
+  );
+};
